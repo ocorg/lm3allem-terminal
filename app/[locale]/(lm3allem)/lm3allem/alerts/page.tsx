6b579d2751ee75ad2a1d07ffa4fd296a334076eb
Jack998899 <oussamachbr@gmail.com>
2026-05-30 11:50:05 +0100

    ph_8_A

--- app/[locale]/(lm3allem)/lm3allem/alerts/page.tsx
+++ app/[locale]/(lm3allem)/lm3allem/alerts/page.tsx
@@ -1,7 +1,7 @@
 import { getAlerts, type AlertsData } from "@/lib/actions/lm3allem/alerts"
 import { AlertsClient } from "@/components/lm3allem/alerts/AlertsClient"
 
-const EMPTY: AlertsData = { lowStock: [], openCaisse: [], openRentals: [], unpaidCredits: [] }
+const EMPTY: AlertsData = { lowStockItems: [], openCaisseSessions: [], openRentals: [], unpaidCredits: [] }
 
 export default async function AlertsPage() {
   let alerts: AlertsData
