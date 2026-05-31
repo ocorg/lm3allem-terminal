055dddb5d309d86f13bd1e35dcb93d4b3fc6a2d3
Jack998899 <oussamachbr@gmail.com>
2026-05-31 21:13:08 +0100

    manifest

--- middleware.ts
+++ middleware.ts
@@ -35,6 +35,6 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest\\.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
   ],
 }
